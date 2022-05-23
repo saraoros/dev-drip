87e1d7c1751fb8d2d786a8dc2e80f1c98a9496df
Sara Oros Gochez <saraorosgochez@Saras-MacBook-Air.local>
2022-05-23 15:06:10 -0700

    Added Products list and Product Screen. Product itself isn't appearing when clicked.

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import products from "../products";
+
+const ProductScreen = ({ match }) => {
+  const product = products.find((params) => params._id === match.params.id);
+  return (
+    <>
+      <Link className="btn btn-dark my-3" to="/">
+        Go Back to Home
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <strong>Price:</strong> ${product.price}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <strong>Description:</strong> {product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Stock:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductScreen;

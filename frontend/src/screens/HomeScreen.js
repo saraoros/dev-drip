e59f91d7c8b0f9985b240dd1b614e499f998e7f1
Sonarie <sonarie1980@gmail.com>
2022-05-24 07:24:43 -0500

    saving before figuring out where the backend went

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import axios from "axios";
+import { listProducts } from "../actions/productactions";
+
+
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
+  const dispatch = useDispatch()
 
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+  const productList = useSelector(state => state.productList)
+  const { loading, error, products } = productList
+
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch]);
+
+
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
+      {loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> : <Row>
         {products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
-      </Row>
+      </Row>}
     </>
   );
 };

6e0d9d9c168b54d3c8cf9cd0b08e19088ece5098
Sonarie <sonarie1980@gmail.com>
2022-05-24 08:31:40 -0500

    section 5 complete

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
-import {productListReducer} from './reducers/'
+import {productListReducer, productDetailsReducer} from './reducers/'
 
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetailsReducer: productDetailsReducer, 
 })
 
 const initialState = {}

9ef4733a4c4f6809a23de68ac55ba54c788d92ae
Paul Gottinger <pgottinger@outlook.com>
2022-05-26 14:20:13 -0500

    products loading

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,27 +1,31 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/devdrip", {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-});
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI || "mongodb://localhost/devdrip",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    );
 
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
+// mongoose.connect(
+//         process.env.MONGO_URI || "mongodb://localhost/devdrip",
+//         {
+//           useUnifiedTopology: true,
+//           useNewUrlParser: true,
 
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(
-//       process.env.MONGO_URI || "mongodb://localhost/devdrip",
-//       {
-//         useUnifiedTopology: true,
-//         useNewUrlParser: true,
+//         }
+//       );
 
-//       }
-//     );
+// export default mongoose.connection;
 
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error(`Error: ${error.message}`);
-//     process.exit(1);
-//   }
-// };
-export default mongoose.connection;
+export default connectDB;

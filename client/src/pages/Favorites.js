7affd732ce01a08009848bc6879ed952f2f27fd5
Sonarie <sonarie1980@gmail.com>
2022-05-29 15:11:17 -0500

    fixed styling of favorites page

--- client/src/pages/Favorites.js
+++ client/src/pages/Favorites.js
@@ -1,37 +1,39 @@
 import React from 'react'
-import {QUERY_USER} from '../utils/queries'
+import { QUERY_USER } from '../utils/queries'
 import { useQuery } from "@apollo/client";
 import ProductItem from '../components/ProductItem';
 import Cart from "../components/Cart";
 
 
-function Favorites () {
-    const {data, loading} = useQuery(QUERY_USER)
+function Favorites() {
+    const { data, loading } = useQuery(QUERY_USER)
     const userData = data?.user || {}
-    console.log (userData)
+    console.log(userData)
 
-    if (loading){
+    if (loading) {
         return (
             <div>
                 <h1>LOADING...</h1>
             </div>
-    
+
         )
     }
     return (
-        <div>
-            <h1>{userData.firstName}'s Favorites</h1>
-            {userData.favorites.map(product=>(
-            <ProductItem
-            key={product._id}
-            _id={product._id}
-            image={product.image}
-            name={product.name}
-            price={product.price}
-            quantity={product.quantity}
-          />
-        ))}
-        <Cart />
+        <div className="container">
+            <h4>{userData.firstName}'s Favorites</h4>
+            <section className="flex-row mb-2" >
+                {userData.favorites.map(product => (
+                    <ProductItem
+                        key={product._id}
+                        _id={product._id}
+                        image={product.image}
+                        name={product.name}
+                        price={product.price}
+                        quantity={product.quantity}
+                    />
+                ))}
+            </section>
+            <Cart />
         </div>
     )
 }

c3884722eadc4984b28669f5dbfe1d41b03e3332
Paul Gottinger <pgottinger@outlook.com>
2022-05-31 15:46:55 -0500

    hashrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
